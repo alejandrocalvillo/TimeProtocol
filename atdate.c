1b9bd9a41fa3d58f3788fd042997cf4df79d504b
alejandrocalvillo <alejandrocalvillo.it@gmail.com>
2021-09-16 10:20:51 +0200

    Vamos avanzando

--- atdate.c
+++ atdate.c
@@ -7,8 +7,19 @@ int port;
 
 void timerProtocol (){
     int sockfd;
+    struct sockaddr_in my_addr;
+    struct sockaddr_in dest_addr;
+    struct hostent *dest_server;
     if(mode==0){//UDP MODE
+        u_int32_t datagram; //VOID DATAGRAM 32 bits
         sockfd=socket(PF_INET, SOCK_DGRAM,0);
+        if (sockfd==-1){
+            perror("Error al crear el SOCKT");
+            exit(0);
+        }
+        dest_server=gethostbyname(servername);
+       // dest_addr.sin_addr.s_addr=dest_server;
+        
     }
 }
 
